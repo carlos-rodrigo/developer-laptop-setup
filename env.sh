5c61dd57f591280e6888350ff0691df9c8d4b9fd
Carlos Rodrigo <53827131+carlos-rodrigo@users.noreply.github.com>
2022-02-06 16:10:29 -0300

    Update env.sh

--- env.sh
+++ env.sh
@@ -11,16 +11,3 @@ function r() { grep "$1" ${@:2} -R . }
 
 # Create a folder and move into it in one command
 function mkcd() { mkdir -p "$@" && cd "$_"; }
-
-# Example aliases
-alias cppcompile='c++ -std=c++11 -stdlib=libc++'
-
-
-# Docker platform
-# export DOCKER_DEFAULT_PLATFORM=linux/amd64
-
-# NVM Setting
-export NVM_DIR="$HOME/.nvm"
-  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
-eval "$(rbenv init -)"
