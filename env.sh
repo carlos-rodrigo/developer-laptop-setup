eee113d6919e9e91978fd8460b0c8ca5503a1e79
Carlos Rodrigo <53827131+carlos-rodrigo@users.noreply.github.com>
2022-02-06 09:37:27 -0300

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+# Add commonly used folders to $PATH
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/homebrew/opt/helm@2/bin/:$PATH"
+# Specify default editor. Possible values: vim, nano, ed etc.
+export EDITOR=vim
+
+# File search functions
+function f() { find . -iname "*$1*" ${@:2} }
+function r() { grep "$1" ${@:2} -R . }
+
+# Create a folder and move into it in one command
+function mkcd() { mkdir -p "$@" && cd "$_"; }
+
+# Example aliases
+alias cppcompile='c++ -std=c++11 -stdlib=libc++'
+
+
+# Docker platform
+# export DOCKER_DEFAULT_PLATFORM=linux/amd64
+
+# NVM Setting
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+eval "$(rbenv init -)"
